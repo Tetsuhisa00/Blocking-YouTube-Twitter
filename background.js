8606c587ae2f58511bfe3f1f783e4b351815b308
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-06-24 08:15:42 -0400

    Update background.js
    Changed the site to be updated from Github's my page to a new tab in Chrome.

--- background.js
+++ background.js
@@ -1,16 +1,15 @@
 
-// ブロックするサイトのURLを指定してください
+//　ブロックするサイトはYouTubeとTwitterに設定
 const blockedSites = ["twitter.com", "youtube.com"];
 
-// タブの更新を監視するイベントリスナーを登録する
+// タブの更新を監視するイベントリスナーを登録
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // タブが更新された場合、新しいURLを取得する
   const url = changeInfo.url || tab.url;
   
-  // ブロックリストにURLが含まれているかチェックする
+  // ブロックリストにURLが含まれているかをチェック
+  // ブロックリストに含まれている場合は、タブをChromeの新しいタブに更新する
   if (blockedSites.some(site => url.includes(site))) {
-    // ブロックされたページにアクセスしようとした場合の処理
-    // chromeのホーム画面(新規タブページのこと)に戻る
     if (url !== "chrome://newtab/") {
       chrome.tabs.update(tabId, { url: "chrome://newtab/" });
     }
