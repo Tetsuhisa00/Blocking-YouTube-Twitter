b7fd9e8fd08c6937fc86e64c2e681d0cd4babe12
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-06-25 16:41:39 +0900

    changed the code by checking all tabs including active tab so as to block all specified tabs that user wants to block.

--- background.js
+++ background.js
@@ -2,17 +2,13 @@
 //　ブロックするサイトはYouTubeとTwitterに設定
 const blockedSites = ["youtube.com", "twitter.com"];
 
-// タブの更新を監視するイベントリスナーを登録
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // タブが更新された場合、新しいURLを取得する
-  const url = changeInfo.url || tab.url;
-  
-  // ブロックリストにURLが含まれているかをチェック
-  // ブロックリストに含まれている場合は、タブをChromeの新しいタブに更新する
-  if (blockedSites.some(site => url.includes(site))) {
-    if (url !== "chrome://newtab/") {
-      chrome.tabs.update(tabId, { url: "chrome://newtab/" });
+chrome.tabs.query({}, function(tabs) {
+  for (var i = 0; i < tabs.length; i++) {
+    var tab = tabs[i];
+    if (blockedSites.some(site => tab.url.includes(site))) {
+      chrome.tabs.update(tab.id, { url: "chrome://newtab/" });
     }
   }
 });
+
 
