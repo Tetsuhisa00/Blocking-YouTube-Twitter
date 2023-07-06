b3b2f1034e73972d36d8bdd4d862e889c5001cb9
Tetsuhisa00 <tetsuhisadevelop@gmail.com>
2023-07-06 22:17:19 +0900

    fixed some type mistakes

--- background.js
+++ background.js
@@ -4,9 +4,9 @@ const blockedSites = ["youtube.com", "twitter.com"];
 
 // 開いている全てのタブをチェックしてブロックリストにあるURLが含まれているかを確認する
 // 含まれている場合は、タブを閉じる
-chrome.tab.query({}, (tabs) => {
+chrome.tabs.query({}, (tabs) => {
   for (let tab of tabs) {
-    if (blockkSites.some(site => tab.url.includes(site))) {
+    if (blockedSites.some(site => tab.url.includes(site))) {
       chrome.tabs.remove(tab.id);
     }
   }
@@ -15,9 +15,9 @@ chrome.tab.query({}, (tabs) => {
 // タブでURLが更新された場合に作動する
 // 更新されたURLがブロックリストに含まれている場合は、タブをChromeの新しいタブに更新する
 chrome.webNavigation.onCommited.addListener((details) => {
-  if (blockkedSites.some(site => details.url.includes(site))) {
+  if (blockedSites.some(site => details.url.includes(site))) {
     if (details.url !== "chrome://newtab/") {
       chrome.tabs.update(details.tabId, { url: "chrome://newtab/" });
     }
   }
-});
+}, {url: [{urlContains: 'youtube.com'}, {urlContains: 'twitter.com'}]});
